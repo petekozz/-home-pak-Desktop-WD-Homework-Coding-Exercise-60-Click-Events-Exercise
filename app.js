4e9ceea52da893c0b8e8bb85b254191e5e815f31
petekozz <66498808+petekozz@users.noreply.github.com>
2020-10-28 20:35:00 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const hello = document.querySelector('#hello');
+hello.addEventListener('click', () => {
+    console.log("hello");
+})
+
+const goodbye = document.querySelector('#goodbye');
+goodbye.addEventListener('click', () => {
+    console.log("goodbye");
+})
